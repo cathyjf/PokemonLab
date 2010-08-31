6ad42a92612524dbf8dcd711b52ca68bbbfcc4ec
Cathy Fitzpatrick <cathy@cathyjf.com>
2010-08-30 22:06:00 -0600

    Made the last commit actually work.

--- src/database/Authenticator.cpp
+++ src/database/Authenticator.cpp
@@ -52,28 +52,34 @@ SECRET_PAIR VBulletinAuthenticator::getSecret(ScopedConnection &conn,
         const string &user, const string &ip) {
     // First check the strike system to make sure the IP is allowed to
     // attempt to log in.
-    Query query = conn->query();
-    query << "SELECT count(*) AS strikes,"
-                "unix_timestamp() - MAX(striketime) AS lasttime "
-            "FROM " << m_database << ".strikes "
-            "WHERE strikeip = %0q "
-                "AND striketime > (unix_timestamp() - 3600)";
-    query.parse();
-    StoreQueryResult result = query.store(ip);
-    const int strikes = result[0][0];
-    const int lastTime = result[0][1];
-    // Note: The following arbitrary constants are directly from the vbulletin
-    //       source, and are not configurable values in vbulletin.
-    if ((strikes >= 5) && (lastTime < 900)) {
-        // This IP has attempted to log in too many times.
-        return SECRET_PAIR();
+    {
+        Query query = conn->query(
+                "SELECT count(*) AS strikes,"
+                    "unix_timestamp() - MAX(striketime) AS lasttime "
+                "FROM " + m_database + ".strikes "
+                "WHERE strikeip = %0q "
+                    "AND striketime > (unix_timestamp() - 3600)"
+            );
+        query.parse();
+        StoreQueryResult result = query.store(ip);
+        const int strikes = result[0][0];
+        const int lastTime = result[0][1].is_null() ? 0 : result[0][1];
+        // Note: The following arbitrary constants are directly from the
+        //       vbulletin source, and are not configurable values in
+        //       vbulletin itself.
+        if ((strikes >= 5) && (lastTime < 900)) {
+            // This IP has attempted to log in too many times.
+            return SECRET_PAIR();
+        }
     }
     
-    query << "SELECT password, salt "
-            "FROM " << m_database << ".user "
-            "WHERE username = %0q";
+    Query query = conn->query(
+            "SELECT password, salt "
+            "FROM " + m_database + ".user "
+            "WHERE username = %0q"
+        );
     query.parse();
-    result = query.store(user);
+    StoreQueryResult result = query.store(user);
     if (result.empty())
         return SECRET_PAIR();
 
@@ -87,17 +93,19 @@ bool VBulletinAuthenticator::finishAuthentication(ScopedConnection &conn,
         const std::string &user, const std::string &ip, const bool success) {
     if (!success) {
         // Give a strike to the IP.
-        Query query = conn->query();
-        query << "INSERT INTO " << m_database << ".strikes "
+        Query query = conn->query(
+                "INSERT INTO " + m_database + ".strikes "
                     "(striketime, strikeip, username) "
-                 "VALUES (unix_timestamp(), %0q, %1q)";
+                 "VALUES (unix_timestamp(), %0q, %1q)"
+             );
         query.parse();
         query.execute(ip, user);
         return false;
     }
 
-    Query query = conn->query();
-    query << "SELECT id FROM " << m_database << ".user WHERE username = %0q";
+    Query query = conn->query(
+            "SELECT userid FROM " + m_database + ".user WHERE username = %0q"
+        );
     query.parse();
     StoreQueryResult result = query.store(user);
     if (result.empty()) {
@@ -105,13 +113,15 @@ bool VBulletinAuthenticator::finishAuthentication(ScopedConnection &conn,
         // "right" time.
         return false;
     }
-    const int foreignId = result[0][1];
+    const int foreignId = result[0][0];
     
-    query << "INSERT INTO users (name, foreign_id, level, activity, ip) "
-            "VALUES (%0q, %1q, 0, unix_timestamp(), %2q) "
-            "ON DUPLICATE KEY UPDATE name=%0q";
-    query.parse();
-    query.execute(user, foreignId, ip);
+    Query q2 = conn->query(
+            "INSERT INTO users (name, foreign_id, level, activity, ip) "
+            "VALUES (%0q, %1q, 0, now(), %2q) "
+            "ON DUPLICATE KEY UPDATE name=%0q, activity=now()"
+        );
+    q2.parse();
+    q2.execute(user, foreignId, ip);
     return true;
 }
 
