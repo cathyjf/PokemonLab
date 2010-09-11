62bca198f3a03b31c41ecc4e8dbf72f6bb1500aa
Cathy Fitzpatrick <cathy@cathyjf.com>
2010-09-11 16:49:46 -0600

    Fixed typo.

--- util/modifiers.rb
+++ util/modifiers.rb
@@ -2,7 +2,7 @@
 
 results = { }
 
-Dir.glob("../resources/*.{js, xml}") { |f| File.open(f) { |io|
+Dir.glob("../resources/*.{js,xml}") { |f| File.open(f) { |io|
     io.readlines.each { |line|
         q = line.chomp.match(/\/\/ *@([^ ]*) +([^,]*), *([^,]*), *([^,]*)/)
         next unless q
@@ -13,7 +13,6 @@ Dir.glob("../resources/*.{js, xml}") { |f| File.open(f) { |io|
 
 results.each { |a, b|
     puts "#{a}:"
-    #puts b.keys.join(",")
     (b.sort { |x, y| x[0] <=> y[0] }).each { |a, b|
         puts "    #{a}: #{b.join(', ')}"
     }
