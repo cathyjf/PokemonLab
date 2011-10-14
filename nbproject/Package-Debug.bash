12b52230014fc30d67087eecbbe117bac81020aa
Martin Pkexp <martin@pokemonexperts.net>
2011-10-14 18:10:06 -0400

    Netbeans Project, Libraries Update
    --HG--
    branch : vmdev-oldjs

--- nbproject/Package-Debug.bash
+++ nbproject/Package-Debug.bash
@@ -9,11 +9,12 @@ TOP=`pwd`
 CND_PLATFORM=GNU-Linux-x86
 CND_CONF=Debug
 CND_DISTDIR=dist
-NBTMPDIR=build/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
+CND_BUILDDIR=build
+NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/shoddybattle2
-OUTPUT_BASENAME=shoddybattle2
-PACKAGE_TOP_DIR=shoddybattle2/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/server
+OUTPUT_BASENAME=server
+PACKAGE_TOP_DIR=server/
 
 # Functions
 function checkReturnCode
@@ -58,15 +59,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/shoddybattle2/bin"
+makeDirectory "${NBTMPDIR}/server/bin"
 copyFileToTmpDir "${OUTPUT_PATH}" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/shoddybattle2.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/server.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/shoddybattle2.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/server.tar *
 checkReturnCode
 
 # Cleanup
